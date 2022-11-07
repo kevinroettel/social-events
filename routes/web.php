9eedd3771853e60b5566a52b7c0dc2d1480bc498
kroettel <kevin.roettel@smail.th-koeln.de>
2022-11-07 14:57:11 +0100

    added wanted routes for user interaction

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Htt\Controllers\ArtistController;
+use App\Htt\Controllers\EventController;
+use App\Htt\Controllers\LocationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artists', [ArtistController::class, 'getArtists']);
+Route::post('/artists', [ArtistController::class, 'createArtist']);
+Route::post('/artists/{artistId}', [ArtistController::class, 'updateArtist']);
+Route::patch('/artists/{artistId}/tags/{tagId}', [ArtistController::class, 'addTagToArtist']);
+Route::delete('/artists/{artistId}/tags/{tagId}', [ArtistController::class, 'removeTagFromArtist']);
+
+Route::get('/events', [EventController::class, 'getEvents']);
+Route::post('/events', [EventController::class, 'createEvent']);
+Route::post('/events/{eventId}', [EventController::class, 'updateEvent']);
+Route::patch('/events/{eventId}/artists/{artistId}', [EventController::class, 'addArtistToEvent']);
+Route::delete('/events/{eventId}/artists/{artistId}', [EventController::class, 'removeArtistFromEvent']);
+
+Route::get('/locations', [LocationController::class, 'getLocations']);
+Route::post('/locations', [LocationController::class, 'createLocation']);
+Route::post('/locations/{locationId}', [LocationController::class, 'updateLocation']);
+
+Route::get('/tags', [TagController::class, 'getTags']);
+Route::post('/tags', [TagController::class, 'createTag']);
+
+// login?
+Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/{userId}', [UserController::class, 'updateProfile']);
+
+Route::patch('/users/{userId}/watchlist/{eventId}/{status}', [WatchlistController::class, 'thinkAboutAGoodName']);
